d87bc32753077b97a74976f00cab385826ffdb50
Eric Liu <eric.liu.developer@gmail.com>
2024-05-17 15:16:22 +1000

    init

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Eric Liu on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
